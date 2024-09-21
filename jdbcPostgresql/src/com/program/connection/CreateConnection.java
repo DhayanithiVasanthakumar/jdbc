3d2307d1955c5d7008e5c155b16daa4f6953c369
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-09-21 21:19:22 +0530

    connection

--- jdbcPostgresql/src/com/program/connection/CreateConnection.java
+++ jdbcPostgresql/src/com/program/connection/CreateConnection.java
@@ -24,7 +24,6 @@ public class CreateConnection {
 		}  catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
